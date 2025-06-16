56beecad63705678808c312839187a79de6ce6a7
arnavgoyal543 <goyaldev2003@gmail.com>
2025-06-16 09:14:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1213,7 +1213,11 @@ function updateReplyUI() {
       <div class="reply-preview">
         <div class="reply-to">Replying to ${replyToName}</div>
         ${replyContent}
-        <button onclick="cancelReply()" class="cancel-reply">×</button>
+        <button onclick="cancelReply()" class="cancel-reply">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
+          </svg>
+        </button>
       </div>
     `;
     replyPreview.classList.remove('hidden');
@@ -1248,4 +1252,11 @@ function scrollToBottom() {
         messagesContainer.scrollTop = messagesContainer.scrollHeight;
     }
 }
+
+// Add event listener for Escape key to cancel reply
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && state.replyingTo) {
+    cancelReply();
+  }
+});
 
